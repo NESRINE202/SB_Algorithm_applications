9125750fcd72ddde5b3b66725ce29600ea79b6b0
Nejma <chahine.nejma@student-cs.fr>
2023-03-13 17:24:02 +0100

    e

--- verifbrute.py
+++ verifbrute.py
@@ -2,7 +2,8 @@ from matrice import M, H, n, pas, iteration, nbrsimulation
 import numpy as np
 
 
-def decimal_to_binary(k, n):
+def decimal_to_binary(k, n): #retourne une liste de longuer n (nombre de particule) avec des 1 et des -1 
+    #k c'est l ordre de la configurationet on a 2**n configuration total
     binary = []
     while k > 0:
         binary = [2*(k % 2)-1] + binary
@@ -10,8 +11,11 @@ def decimal_to_binary(k, n):
     return (n - len(binary)) * [0] + binary
 
 
-def calculeforce(M,t):
+def calculeforce(M,t):# calcue l'energie avec une configuration donnéé en t
     return sum([M[i,j]*t[i]*t[j] for i in range(n) for j in range(n)])
+
+
+# parcour tt les cas pour avoir le min
 
 best_t1 = [0] * n
 min_value=calculeforce(M,best_t1)
