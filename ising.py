0bfc1d3c8ab27c67e1153e97646080933b23696a
Nejma <chahine.nejma@student-cs.fr>
2023-03-13 16:47:39 +0100

    sexy times2

--- ising.py
+++ ising.py
@@ -2,15 +2,17 @@ import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
 import copy
-
+from matrice import M
+from matrice import H
 class IsingModel:
-    def __init__(self, n, pas, iteration,M,nbrsimulation):
+    def __init__(self, n, pas, iteration,M,H,nbrsimulation):
         #n nombre de particule
         #M matrice des forces d'interactio,
         self.n = n
         self.pas = pas
         self.iteration = iteration
         self.M =M
+        self.H=H
         self.nbrsimulation=nbrsimulation
 
     def calcule_energie(self, t):
@@ -19,15 +21,15 @@ class IsingModel:
     
     def force(self, t, i):
         def sign(x):
-            criteremin=1 #on pourra changer sa
+            criteremin=1 #on pourra changer
             if abs(x)<criteremin:
                 return x/criteremin
             else:
                 return int(x>0)-int(x<0)
         
-        f = -sum([self.M[i,j]*sign(t[j] )for j in range(self.n)]+[self.M[i,i]*sign(t[i])])
+        f = -sum([self.M[i,j]*sign(t[j] )for j in range(self.n)]+[self.H[i]*sign(t[i])])
         return f
-
+    
     def nouvelle_variable(self, t, vitesse):
         # c'est pour calculer la nouvelle position de la particule
         t1 = copy.deepcopy(t)
@@ -41,31 +43,38 @@ class IsingModel:
         # donne le signe de chaque particule
         return [1 if t[i]>0 else -1 for i in range(self.n)]
 
+        
     def simulate(self):
+        
         
         for i in range(self.nbrsimulation):
             position=[rd.randint(0,1)*2-1 for i in range(self.n)]
             vitesse=[0]*self.n
-
+            posparhistorique=[[k] for k in position]
             E=[]    
-            for i in range(self.iteration):
+            for a in range(self.iteration):
                 E.append(self.calcule_energie(self.signage(position)))
                 position, vitesse = self.nouvelle_variable(position, vitesse)
-
+                for j in range(len(posparhistorique)):
+                    posparhistorique[j].append(position[j])
+            
             Y=[i for i in range(len(E))]
             plt.plot(Y, E)
             print(self.signage(position))
+            
         plt.show()
+
         
 # Utilisation de la classe IsingModel
-
+print("je commence")
 n = 10
 
 pas = 0.001
 
-iteration = 100000
+iteration = 10000
 
-nbrsimulation=10
+nbrsimulation=15
+H=np.zeros(n)
 I=np.eye((n))
 M=np.zeros((n,n))
 for i in range(n):
@@ -74,6 +83,8 @@ for i in range(n):
                 M[i,j] = c
                 M[j,i] = c
                 
+for i in range(n):
+    H[i]=rd.randint(0,1)
 
-ising_model = IsingModel(n, pas, iteration,M,nbrsimulation)
+ising_model = IsingModel(n, pas, iteration,M,H,nbrsimulation)
 ising_model.simulate()
---
+++ matrice.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random as rd
+
+n = 10
+pas = 0.001
+iteration = 10000
+nbrsimulation=15
+M=np.zeros((n,n))
+for i in range(n):
+            for j in range(i+1):
+                c = rd.randint(0,9)
+                M[i,j] = c
+                M[j,i] = c
+H=np.zeros(n)
+for i in range(n):
+    H[i]=rd.randint(0,1)
