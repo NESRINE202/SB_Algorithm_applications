6fce946d1a5e6faed8307b3efe2e9cd7e083c10c
nesrine_chiba <nessrine.chiba2@student-cs.fr>
2023-03-23 19:54:50 +0100

    nn

---
+++ matrice.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random as rd
+
+n = 10
+pas = 0.001
+iteration = 10000
+nbrsimulation=n
+M=np.zeros((n,n))
+for i in range(n):
+            for j in range(i+1):
+                if i!=j:
+                    c = rd.randint(0,9)
+                    M[i,j] = c
+                    M[j,i] = c
+H=np.zeros(n)
+for i in range(n):
+    H[i]=rd.randint(0,1)
+ 
---
+++ verifbrute.py
@@ -0,0 +1,24 @@
+from matrice import n, pas, iteration, nbrsimulation
+import numpy as np
+from ising import M,H
+
+
+
+
+configurations = np.array([[-1] * n])
+for i in range(2**n-1):
+    binary = np.array(list(bin(i+1)[2:].zfill(n)), dtype=int)
+    configurations = np.vstack((configurations, 2*binary-1))
+
+energies = np.dot(configurations, np.dot(M, configurations.T)).diagonal() + np.dot(configurations, H)
+
+best_t1 = configurations[np.argmin(energies)]
+min_value = energies.min()
+
+print(best_t1)
+print(min_value)
+print("M=",M)
+print("H=",H)
+
+
+     
