fcb0252431c993e88dbf50d4527aba03ad831e60
nesrine_chiba <nessrine.chiba2@student-cs.fr>
2023-03-31 16:45:15 +0200

    j

--- bifurcation.py
+++ bifurcation.py
@@ -19,14 +19,16 @@ def Tracage_bifurcation(delta, phi0, M, K, T, N):
     h = T / (N+1)
     def calcul_energie(X,Y,k):
         s = 0
+        """"
         for i in range(n):
             si = 0
             for j in range(n):
                 si+= M[i,j]*X[k,j]
 
-            s += - 0.25*si*X[k,i]
-            #delta*0.5*Y[k,i]**2 +K*0.25*X[k,i]**2+(delta-p(k*h))/2*X[k,i]**2
-        return s 
+            s += - 0.25*si*X[k,i]"""
+        for i in range(n):
+           s +=delta*0.5*Y[k,i]**2 #+K*0.25*X[k,i]**2+(delta-p(k*h))/2*X[k,i]**2
+        return s  
     
     def calculate_sum_of_column(X, k):
         """Calculate the sum of the k-th column of X array."""
@@ -47,10 +49,12 @@ def Tracage_bifurcation(delta, phi0, M, K, T, N):
     Temps = np.linspace(0, T, N+1)  # fix the linspace call
     P = [p(t) for t in Temps ]
     E = [calcul_energie(X,Y,k) for k in range(N+1)]
+
     #plt.plot(Temps,P,label='P(t)')
     plt.plot(Temps,E,label ='E(t)')
-    plt.plot(Temps, X[:,1],label = 'X2(t)')  # plot the first column of X
-    plt.plot(Temps,X[:,0],label='X1(t)')
+    #plt.plot(Temps, X[:,1],label = 'X2(t)')  # plot the first column of X
+    #plt.plot(Temps,X[:,0],label='X1(t)')
+
     plt.legend()
     plt.show()
 
