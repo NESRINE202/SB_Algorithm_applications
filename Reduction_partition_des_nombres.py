3d9f21c7cbb021f5bec44388ca2a5c5e8685bf7a
nesrine_chiba <nessrine.chiba2@student-cs.fr>
2023-04-10 01:44:01 +0200

    m

--- Reduction_partition_des_nombres.py
+++ Reduction_partition_des_nombres.py
@@ -1,10 +1,10 @@
 import numpy as np 
-
+from ising import *
 
 class Sac():
     def __init__(self,capacite,signe) -> None:
         self.capacite = capacite 
-        self.signe = signe
+        self.signe = signe # correpond au code donne si on choisi le premier ou le deuxime sac
 
 class Item(): 
     def __init__(self,poids) -> None:
@@ -14,10 +14,45 @@ class Item():
         self.sac = sac 
 
 
+class Reduction(): 
+    def __init__(self,Nombres_item,Items,Sacs) -> None:
+        self.Nombres_item= Nombres_item
+        self.Items = Items # an array that  the weight of the items
+        self.Sacs = Sacs #un tableau qui contient la capcité de chaque sac 
+
+
+    def reduction(self): 
+        # construction de la Matrice J du modèle d'ising 
+        n = len(self.Nombres_item)
+        J= np.zeros(n,n)
+        for i in range(n): 
+            for j in range(n): 
+                if i != j : 
+                    J[i,j]=self.Items[i]*self.Items[j]
+                    J[j,i]= J[i,j]
+                else:
+                    J[i,i]=0
+        self.J = J 
+
+
+    def partition(self): 
+        #l'algorithme de bifuraction à implementer 
+        repartition = IsingModel.simulate()
+        # Je suppose ici que Simulate retourne un tableau d'un signe qui correspond a chacun des sac qui tient compte des deux sac 
+        # on peut faire la somme des poids et prendre deux qui ont le mem poids 
+    
+        self.repartition = repartition 
+
+                
+
+
+
+
 
 # def main():
 
 
 
 # if __name__ == "__main__":
-#     main()
+     #main()    
+          
