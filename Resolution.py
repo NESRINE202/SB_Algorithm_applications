5b7854e6bc7032ba6c122b1f893ca9835d90c27a
nesrine_chiba <nessrine.chiba2@student-cs.fr>
2023-04-10 13:57:27 +0200

    d

--- Resolution.py
+++ Resolution.py
@@ -5,6 +5,7 @@ def main():
 
     repartition = s
     print("repartition= ",repartition)
+    # Il reste à bien faire le lien avec les sacs et jouer sur les contarintes 
 
 
 if __name__ == "__main__":
