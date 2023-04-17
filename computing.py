23b50762156894dca5fb7acf5249881fb716629c
Mulder Nolan <nolan.mulder@student-cs.fr>
2023-04-17 16:43:43 +0200

    Added an option to not save the instance and simulation results to a file and a new (intereting?) graph.

--- computing.py
+++ computing.py
@@ -6,18 +6,20 @@ import datetime
 # This file is for doing the actual simulation and saving the results to be latter annalysed
 
 # compute for a sigle instance
-def compute_single_instance(instance_size, step, n_itterations, n_cond_init, J, H):
+def compute_single_instance(instance_size, step, n_itterations, n_cond_init, J, H, savetofile=True):
     # run the algorithm
     ising_model = IsingModel(step, n_itterations, n_cond_init, J, H)
     states, energies = ising_model.simulate()
 
     # Save all the data (parameters, instance, results) inside of a file
     # Contains the parameters, the instance matrix and the states and enrgies matrices
-    now = datetime.datetime.now()
-    datestr = now.strftime("%d%m%Y-%H%M")
-    filename = f"{instance_size}_{step}_{n_itterations}_{n_cond_init}_{datestr}.npz"
-    path = f'computing_results/{filename}'
-    np.savez(path, J=J, H=H, states=states, energies=energies)
+    path = ''
+    if savetofile:
+        now = datetime.datetime.now()
+        datestr = now.strftime("%d%m%Y-%H%M")
+        filename = f"{instance_size}_{step}_{n_itterations}_{n_cond_init}_{datestr}.npz"
+        path = f'computing_results/{filename}'
+        np.savez(path, J=J, H=H, states=states, energies=energies)
 
     return states, energies, path
 
--- results_annalysis.py
+++ results_annalysis.py
@@ -35,6 +35,20 @@ def plot_energies_evolution(energies):
 
     plt.show()
 
+def plot_min_energy_evolution(energies):
+    n_iterration = len(energies[0])
+    abcisses = np.arange(n_iterration)
+
+    min_energies = energies.min(axis=0)
+
+    plt.plot(abcisses, min_energies)
+
+    plt.xlabel("Iteration number")
+    plt.ylabel("Minum of energy reached at itteration $t$")
+    plt.title("Evolution of the minimum of energy reached across all simulations.")
+
+    plt.show()
+
 # Histogram of the minimum energies reached by each simulation
 def plot_energies_hist(energies):
     minimums = energies.min(axis=1)
