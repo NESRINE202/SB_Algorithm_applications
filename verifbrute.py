28f474c2c53ab8dcebddac1aca4a81daccefae2f
Nejma <chahine.nejma@student-cs.fr>
2023-04-17 16:31:03 +0200

    e

--- verifbrute.py
+++ verifbrute.py
@@ -1,17 +1,17 @@
-def verif_function (M,H):
-    
-    import numpy as np
-    
+import numpy as np
+
+#import ising_copy
+def verif_function(M,H):
     n=M.shape[0]
+    def calcule_energie(M,H,t):
+        return np.dot(t,np.dot(M,t)) +np.dot(H,t)
     
     def decimal_to_binary(k, n):
-    # Return a list of length n with 1's and -1's representing the binary digits of k
+        # Return a list of length n with 1's and -1's representing the binary digits of k
         binary_str = format(k, 'b').zfill(n)
+    
         return [-1 if bit == '0' else 1 for bit in binary_str]
 
-    def calcule_energie(Matrice,t,H):
-        return np.dot(t,np.dot(Matrice,t))+ np.dot(H,t)
-    
 
 
     configurations = np.array([[-1] * n])
@@ -24,18 +24,17 @@ def verif_function (M,H):
 # parcour tt les cas pour avoir le min
 
     best_t1 = [-1] * n
-    min_value=calcule_energie(M,best_t1,H)
+    min_value=calcule_energie(M,best_t1)
 
 
     for i in range(2 ** n):
         t1 = decimal_to_binary(i, n)
-        m = calcule_energie(M,t1,H)
+        m = calcule_energie(M,t1)
         if m < min_value:
             min_value = m
             best_t1 = t1
 
     print(best_t1)
     print(min_value)
-
 
     
