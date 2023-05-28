Fixed the random instance generation witch didn't generate a symetrical matrix. Fixed the signs of the implemented equations in ising.py (doesn't change anything). Added something that fixes the issue where the energies diverge upwards, comming from a reduction of an NPP instance, diverge upwards. This was because we didn't cap the positions inputted in the force computing. This resulted in forces not being caped and energies diverging. But I can't explain why this problem only apeared with the NPP reduction yet.

instance_genrerantion.py
@@ -2,7 +2,15 @@ import numpy as np
 import random as rd
 
 def generate_instance(size):
-       M = np.random.uniform(low=0, high=10, size=(size, size))
+       # Generate random normal coefficients
+       coefficients = np.random.normal(size=(size, size))
+
+       # Set diagonal elements to zero
+       np.fill_diagonal(coefficients, 0)
+
+       # Generate symmetric matrix
+       M = (coefficients + coefficients.T) / 2
+
        H = np.zeros(size)
 
        return M, H

ising_model.py
@@ -49,7 +49,9 @@ class IsingModel:
         for t in range(1, self.iteration):
             # positions et vitesses pour toutes les conditions initiales a l'instant t
             prev_positions, prev_speeds = states[:, :, t-1, 0], states[:, :, t-1, 1]
-            states[:, :, t, 0], states[:, :, t, 1] = self.simplectic_update_forall_simulations(prev_positions, prev_speeds, t) # current_positions, current_speeds
+            signed_prev_positions = np.where(prev_positions>1, 1, prev_positions)
+            signed_prev_positions = np.where(prev_positions<-1, -1, signed_prev_positions)
+            states[:, :, t, 0], states[:, :, t, 1] = self.simplectic_update_forall_simulations(signed_prev_positions, prev_speeds, t) # current_positions, current_speeds
             current_positions = states[:, :, t, 0]
 
             # calcul des énergies

problem_reductions/npp_to_ising.py
@@ -2,7 +2,8 @@ import numpy as np
 
 # Creating the interation matrix from a given NPP instance
 def npp_to_ising(weights):
-    J = -np.outer(weights, weights)
+    J = np.outer(weights, weights)
+    np.fill_diagonal(J, 0)
     H = np.zeros(len(weights))
     return J, H
 